6657f87f9b11e8e3394a64db776f30bd7063ed39
Bak David <bak.david96@gmail.com>
2018-11-06 12:37:47 +0200

    Navigation bar and icons added to resources

---
+++ app/src/main/java/com/example/pedrohuan/sapinewsandroidappv2/application/CreateNewsActivity.java
@@ -0,0 +1,15 @@
+package com.example.pedrohuan.sapinewsandroidappv2.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pedrohuan.sapinewsandroidappv2.R;
+
+public class CreateNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_news);
+    }
+}
