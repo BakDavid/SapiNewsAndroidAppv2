e748baed5058955902b5154492cd69aac316e29a
Bak David <bak.david96@gmail.com>
2018-11-06 11:47:06 +0200

    Login and registration made with Firebase database

--- app/src/main/java/com/example/pedrohuan/sapinewsandroidappv2/MainActivity.java
+++ app/src/main/java/com/example/pedrohuan/sapinewsandroidappv2/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.pedrohuan.sapinewsandroidappv2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.pedrohuan.sapinewsandroidappv2.application.ListNewsActivity;
+import com.example.pedrohuan.sapinewsandroidappv2.authentication.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
+        if(currentUser == null)
+        {
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            Intent intent = new Intent(MainActivity.this,ListNewsActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/example/pedrohuan/sapinewsandroidappv2/authentication/LoginActivity.java
@@ -0,0 +1,116 @@
+package com.example.pedrohuan.sapinewsandroidappv2.authentication;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.pedrohuan.sapinewsandroidappv2.R;
+import com.example.pedrohuan.sapinewsandroidappv2.application.ListNewsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText editEmail;
+    EditText editPassword;
+
+    String mEmail;
+    String mPassword;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editEmail = (EditText) findViewById(R.id.email_input);
+        editPassword = (EditText) findViewById(R.id.password_input);
+
+        findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEmail = editEmail.getText().toString();
+                mPassword = editPassword.getText().toString();
+
+                loginToApp(mEmail,mPassword);
+            }
+        });
+
+        findViewById(R.id.register_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+                finish();
+            }
+        });
+    }
+
+
+    private void loginToApp(String email,String password)
+    {
+        if(validateData(email,password))
+        {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                //FirebaseUser user = mAuth.getCurrentUser();
+                                //updateUI(user);
+
+                                Intent listIntent = new Intent(LoginActivity.this,ListNewsActivity.class);
+                                startActivity(listIntent);
+                                finish();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+        }
+
+
+    }
+
+    private boolean validateData(String email, String password)
+    {
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+
+        if(email.trim().isEmpty())
+        {
+            Toast.makeText(LoginActivity.this,"No email entered!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(password.trim().isEmpty())
+        {
+            Toast.makeText(LoginActivity.this,"No password entered!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(!pattern.matcher(email).matches())
+        {
+            Toast.makeText(LoginActivity.this,"Email typed wrong!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+}
